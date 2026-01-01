5e3fd201f2c60b565e722f0a37bc02e316547b5a
Chanbasha30 <javaeduacademy.info@gmail.com>
2026-01-01 19:58:52 +0530

    Update Test.java

--- src/jea/test/ex1/Test.java
+++ src/jea/test/ex1/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         Integer[] numbers = {5, 2, 9, 1, 7};
         //before sort
-        Arrays.sort(numbers);
+       // Arrays.sort(numbers);
 
         System.out.println("Original array: " + Arrays.toString(numbers));
 
