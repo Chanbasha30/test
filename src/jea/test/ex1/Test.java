67992f4340ce6cb0b39e5c7bc2cd1884b1d3ba68
chanbasha <chanbahsa@protonmail.com>
2026-01-01 17:46:09 +0530

    adding extra sysout

--- src/jea/test/ex1/Test.java
+++ src/jea/test/ex1/Test.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         Integer[] numbers = {5, 2, 9, 1, 7};
+        //before sort
+        Arrays.sort(numbers);
 
         System.out.println("Original array: " + Arrays.toString(numbers));
 
